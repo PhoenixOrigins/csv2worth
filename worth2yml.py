d305b9cb8468881b38e72f2f019c39001896a91e
jstnf <mcpokeball@gmail.com>
2019-07-09 04:16:25 -0700

    Initial commit

---
+++ worth2yml.py
@@ -0,0 +1,16 @@
+def main():
+	ecofile = open("economy.csv", "r")
+	ecolines = ecofile.readlines()
+	ecofile.close()
+	
+	worthfile = open("worth.yml", "w+")
+	for x in ecolines:
+	  line = x
+	  comma_index = line.find(",")
+	  item = line[:comma_index]
+	  worth = line[comma_index + 1:]
+	  worthfile.write("%s: %s" % (item.upper(), worth))
+	worthfile.close()
+
+if __name__ == "__main__":
+  main()
