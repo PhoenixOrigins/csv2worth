ebc5354b32cb59e39a2fbd70d05a252589769ea4
jstnf <mcpokeball@gmail.com>
2019-08-28 03:59:09 -0700

    Survival economy changes

--- csv2worth.py
+++ csv2worth.py
@@ -5,18 +5,18 @@ def main():
     print("------------------------")
 	
     print("Grabbing economy.csv...")
-    ecofile = open("economy.csv", "r")
+    ecofile = open("economysurvival.csv", "r")
     ecolines = ecofile.readlines()
     ecofile.close()
 
     print("Generating worth.yml file...\n")
-    worthfile = open("worth.yml", "w+", newline="")
+    worthfile = open("worthsurvival.yml", "w+", newline="")
     worthfile.write("worth:\n")
     for x in ecolines:
       line = x
       comma_index = line.find(",")
       if comma_index != -1:
-        item = line[:comma_index].upper()
+        item = line[:comma_index].lower()
         worth = line[comma_index + 1:]
         print("Found item %s worth %s" % (item, worth), end="")
         worthfile.write("  %s: %s" % (item, worth))
